45f7959f84dbb1427e4e6174ce4b9b45f5d2f305
Shota Higuchi <hgsyo76@gmail.com>
2019-09-14 15:05:05 +0900

    [update] Change to Serial.println

--- sample5-LightSensor/sample5-LightSensor.ino
+++ sample5-LightSensor/sample5-LightSensor.ino
@@ -7,6 +7,6 @@ Serial.begin(9600); //シリアル通信のデータ転送レートを9600bpsで
 
 void loop(){
 val = analogRead(0); //A0ピンの値を読み取り
-Serial.print(val); //読み取り値を表示
+Serial.println(val); //読み取り値を表示
 delay(100); //100m秒（0.1秒)ごとに表示させるための遅延
 }
